c8461e4e7cbceed43995d4a2cb5e62a8fb16df3f
Mirthe <mirtheklaassen@outlook.com>
2023-01-25 20:17:12 +0100

    Quickly added all example data, descriptions will be updated later

---
+++ R/data.R
@@ -0,0 +1,249 @@
+#' physmap6
+#'
+#' An original SBtab file as provided by the SysRev platform
+#'
+#' @format ## `physmap6`
+#' A data frame with 14 rows and 6 columns:
+#' \describe{
+#'   \item{Compounds}{Compound ID's}
+#'   ...
+#' }
+#' @source <data-raw/physmap6.tsv>
+"physmap6"
+
+#' physmap7
+#'
+#' An original SBtab file as provided by the SysRev platform
+#'
+#' @format ## `physmap7`
+#' A data frame with 14 rows and 6 columns:
+#' \describe{
+#'   \item{Compounds}{Compound ID's}
+#'   ...
+#' }
+#' @source <data-raw/physmap7.tsv>
+"physmap7"
+
+#' physmap8
+#'
+#' An original SBtab file as provided by the SysRev platform
+#'
+#' @format ## `physmap8`
+#' A data frame with 14 rows and 6 columns:
+#' \describe{
+#'   \item{Compounds}{Compound ID's}
+#'   ...
+#' }
+#' @source <data-raw/physmap8.tsv>
+"physmap8"
+
+#' physmap9
+#'
+#' An original SBtab file as provided by the SysRev platform
+#'
+#' @format ## `physmap9`
+#' A data frame with 14 rows and 6 columns:
+#' \describe{
+#'   \item{Compounds}{Compound ID's}
+#'   ...
+#' }
+#' @source <data-raw/physmap9.tsv>
+"physmap9"
+
+#' physmap6_species
+#'
+#' Result of putting physmap6 through sep.sbtab and prep.merge
+#'
+#' @format ## `physmap6_species`
+#' A data frame with 14 rows and 6 columns:
+#' \describe{
+#'   \item{Compounds}{Compound ID's}
+#'   \item{Products}{Product ID's}
+#'   \item{ID}{Reaction ID}
+#'   \item{Compoundname}{Compound name}
+#'   \item{Productname}{Product name}
+#'   \item{Productname}{Name of original file}
+#'   ...
+#' }
+#' @source <data-raw/physmap6.tsv>
+"physmap6_species"
+
+#' physmap6_edges
+#'
+#' Result of putting physmap6 through sep.sbtab and prep.merge
+#'
+#' @format ## `physmap6_edges`
+#' A data frame with 14 rows and 6 columns:
+#' \describe{
+#'   \item{Compounds}{Compound ID's}
+#'   \item{Products}{Product ID's}
+#'   \item{ID}{Reaction ID}
+#'   \item{Compoundname}{Compound name}
+#'   \item{Productname}{Product name}
+#'   \item{Productname}{Name of original file}
+#'   ...
+#' }
+#' @source <data-raw/physmap6.tsv>
+"physmap6_edges"
+
+#' physmap6_compartments
+#'
+#' Result of putting physmap6 through sep.sbtab and prep.merge
+#'
+#' @format ## `physmap6_compartments`
+#' A data frame with 14 rows and 6 columns:
+#' \describe{
+#'   \item{Compounds}{Compound ID's}
+#'   \item{Products}{Product ID's}
+#'   \item{ID}{Reaction ID}
+#'   \item{Compoundname}{Compound name}
+#'   \item{Productname}{Product name}
+#'   \item{Productname}{Name of original file}
+#'   ...
+#' }
+#' @source <data-raw/physmap6.tsv>
+"physmap6_compartments"
+
+#' prepped_physmap6_reactions
+#'
+#' Result of putting physmap6 through sep.sbtab and prep.merge
+#'
+#' @format ## `prepped_physmap6_reactions`
+#' A data frame with 14 rows and 6 columns:
+#' \describe{
+#'   \item{Compounds}{Compound ID's}
+#'   \item{Products}{Product ID's}
+#'   \item{ID}{Reaction ID}
+#'   \item{Compoundname}{Compound name}
+#'   \item{Productname}{Product name}
+#'   \item{Productname}{Name of original file}
+#'   ...
+#' }
+#' @source <data-raw/physmap6.tsv>
+"prepped_physmap6_reactions"
+
+#' prepped_physmap7_reactions
+#'
+#' Result of putting physmap7 through sep.sbtab and prep.merge
+#'
+#' @format ## `prepped_physmap7_reactions`
+#' A data frame with 14 rows and 6 columns:
+#' \describe{
+#'   \item{Compounds}{Compound ID's}
+#'   \item{Products}{Product ID's}
+#'   \item{ID}{Reaction ID}
+#'   \item{Compoundname}{Compound name}
+#'   \item{Productname}{Product name}
+#'   \item{Productname}{Name of original file}
+#'   ...
+#' }
+#' @source <data-raw/physmap7.tsv>
+"prepped_physmap7_reactions"
+
+#' merged_physmap67_edges
+#'
+#' Result of putting physmap7 through sep.sbtab and prep.merge
+#'
+#' @format ## `merged_physmap67_edges`
+#' A data frame with 14 rows and 6 columns:
+#' \describe{
+#'   \item{Compounds}{Compound ID's}
+#'   \item{Products}{Product ID's}
+#'   \item{ID}{Reaction ID}
+#'   \item{Compoundname}{Compound name}
+#'   \item{Productname}{Product name}
+#'   \item{Productname}{Name of original file}
+#'   ...
+#' }
+#' @source <data-raw/physmap7.tsv>
+"merged_physmap67_edges"
+
+#' merged_physmap67_species
+#'
+#' Result of putting physmap7 through sep.sbtab and prep.merge
+#'
+#' @format ## `merged_physmap67_species`
+#' A data frame with 14 rows and 6 columns:
+#' \describe{
+#'   \item{Compounds}{Compound ID's}
+#'   \item{Products}{Product ID's}
+#'   \item{ID}{Reaction ID}
+#'   \item{Compoundname}{Compound name}
+#'   \item{Productname}{Product name}
+#'   \item{Productname}{Name of original file}
+#'   ...
+#' }
+#' @source <data-raw/physmap7.tsv>
+"merged_physmap67_species"
+
+#' merged_physmap67_compartments
+#'
+#' Result of putting physmap7 through sep.sbtab and prep.merge
+#'
+#' @format ## `merged_physmap67_compartments`
+#' A data frame with 14 rows and 6 columns:
+#' \describe{
+#'   \item{Compounds}{Compound ID's}
+#'   \item{Products}{Product ID's}
+#'   \item{ID}{Reaction ID}
+#'   \item{Compoundname}{Compound name}
+#'   \item{Productname}{Product name}
+#'   \item{Productname}{Name of original file}
+#'   ...
+#' }
+#' @source <data-raw/physmap7.tsv>
+"merged_physmap67_compartments"
+
+#' merged_physmap6789_edges
+#'
+#' Result of putting physmap7 through sep.sbtab and prep.merge
+#'
+#' @format ## `merged_physmap6789_edges`
+#' A data frame with 14 rows and 6 columns:
+#' \describe{
+#'   \item{Compounds}{Compound ID's}
+#'   \item{Products}{Product ID's}
+#'   \item{ID}{Reaction ID}
+#'   \item{Compoundname}{Compound name}
+#'   \item{Productname}{Product name}
+#'   \item{Productname}{Name of original file}
+#'   ...
+#' }
+#' @source <data-raw/physmap7.tsv>
+"merged_physmap6789_edges"
+
+#' merged_physmap6789_species
+#'
+#' Result of putting physmap7 through sep.sbtab and prep.merge
+#'
+#' @format ## `merged_physmap6789_species`
+#' A data frame with 14 rows and 6 columns:
+#' \describe{
+#'   \item{Compounds}{Compound ID's}
+#'   \item{Products}{Product ID's}
+#'   \item{ID}{Reaction ID}
+#'   \item{Compoundname}{Compound name}
+#'   \item{Productname}{Product name}
+#'   \item{Productname}{Name of original file}
+#'   ...
+#' }
+#' @source <data-raw/physmap7.tsv>
+"merged_physmap6789_species"
+
+#' merged_physmap6789_compartments
+#'
+#' Result of putting physmap7 through sep.sbtab and prep.merge
+#'
+#' @format ## `merged_physmap6789_compartments`
+#' A data frame with 14 rows and 6 columns:
+#' \describe{
+#'   \item{Compounds}{Compound ID's}
+#'   \item{Products}{Product ID's}
+#'   \item{ID}{Reaction ID}
+#'   \item{Compoundname}{Compound name}
+#'   \item{Productname}{Product name}
+#'   \item{Productname}{Name of original file}
+#'   ...
+#' }
+#' @source <data-raw/physmap7.tsv>
+"merged_physmap6789_compartments"
