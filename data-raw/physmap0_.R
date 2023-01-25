e239280759c1e6e14a922ce56048760e516906c4
Mirthe <mirtheklaassen@outlook.com>
2023-01-25 14:53:58 +0100

    changed functionnames

--- data-raw/physmap0_.R
+++ data-raw/physmap0_.R
@@ -1,6 +1,6 @@
 ## code to prepare `physmap*_` files as found in data/compartments, data/edges, data/full, data/reactions and data/species
-sep.sbtab("data-raw/physmap6.tsv", "physmap6", "data")
-sep.sbtab("data-raw/physmap7.tsv", "physmap7", "data")
-sep.sbtab("data-raw/physmap8.tsv", "physmap8", "data")
-sep.sbtab("data-raw/physmap9.tsv", "physmap9", "data")
-sep.sbtab("data-raw/physmap10.tsv", "physmap10", "data")
+sep_sbtab("data-raw/physmap6.tsv", "physmap6", "data")
+sep_sbtab("data-raw/physmap7.tsv", "physmap7", "data")
+sep_sbtab("data-raw/physmap8.tsv", "physmap8", "data")
+sep_sbtab("data-raw/physmap9.tsv", "physmap9", "data")
+sep_sbtab("data-raw/physmap10.tsv", "physmap10", "data")
