a14129039ea8479c8b8ef0dd4baf0bbde3a34e73
Mirthe <mirtheklaassen@outlook.com>
2023-01-27 12:21:59 +0100

    Documented the data files

---
+++ R/extdata.R
@@ -0,0 +1,189 @@
+#' Original SBtab file 'physmap6'
+#'
+#' This is a .tsv file containing the original physmap6 SBtab
+#' It came from the SysRev platform and was provided to this project by M. Teunis
+#'
+#' @name physmap6
+#'
+#' @section physmap6.tsv:
+#'
+#' This data is used in sep_sbtab() and merge_sbtab().
+NULL
+
+#' Original SBtab file 'physmap7'
+#'
+#' This is a .tsv file containing the original physmap7 SBtab
+#' It came from the SysRev platform and was provided to this project by M. Teunis
+#'
+#' @name physmap7
+#'
+#' @section physmap7.tsv:
+#'
+#' This data is used in sep_sbtab() and merge_sbtab().
+NULL
+
+#' Original SBtab file 'physmap8'
+#'
+#' This is a .tsv file containing the original physmap8 SBtab
+#' It came from the SysRev platform and was provided to this project by M. Teunis
+#'
+#' @name physmap8
+#'
+#' @section physmap8.tsv:
+#'
+#' This data is used in sep_sbtab() and merge_sbtab().
+NULL
+
+#' Original SBtab file 'physmap9'
+#'
+#' This is a .tsv file containing the original physmap9 SBtab
+#' It came from the SysRev platform and was provided to this project by M. Teunis
+#'
+#' @name physmap9
+#'
+#' @section physmap9.tsv:
+#'
+#' This data is used in sep_sbtab() and merge_sbtab().
+NULL
+
+#' The merged compartments tables from SBtab files physmap6, 7, 8 and 9
+#'
+#' This is a .rds file containing all the compartments data of four SBtab files
+#' The original SBtab files are physmap6, physmap7, physmap8 and physmap9
+#' The file has been created using the merge_sbtab() function
+#'
+#' @name merged_physmap6789_compartments
+#'
+#' @section merged_physmap6789_compartments.rds:
+#'
+#' This data is used in merged_net_graph().
+NULL
+
+#' The merged edges tables from SBtab files physmap6, 7, 8 and 9
+#'
+#' This is a .rds file containing all the edges data of four SBtab files
+#' The original SBtab files are physmap6, physmap7, physmap8 and physmap9
+#' The file has been created using the merge_sbtab() function
+#'
+#' @name merged_physmap6789_edges
+#'
+#' @section merged_physmap6789_edges.rds:
+#'
+#' This data is used in merged_net_graph().
+NULL
+
+#' The merged species tables from SBtab files physmap6, 7, 8 and 9
+#'
+#' This is a .rds file containing all the species data of four SBtab files
+#' The original SBtab files are physmap6, physmap7, physmap8 and physmap9
+#' The file has been created using the merge_sbtab() function
+#'
+#' @name merged_physmap6789_species
+#'
+#' @section merged_physmap6789_species.rds:
+#'
+#' This data is used in merged_net_graph().
+NULL
+
+#' The merged compartments tables from SBtab files physmap6 and physmap7
+#'
+#' This is a .rds file containing all the compartments data of two SBtab files
+#' The original SBtab files are physmap6 and physmap7
+#' The file has been created using the merge_sbtab() function
+#'
+#' @name merged_physmap67_compartments
+#'
+#' @section merged_physmap67_compartments.rds:
+#'
+#' This data is used in merged_net_graph().
+NULL
+
+#' The merged edges tables from SBtab files physmap6 and physmap7
+#'
+#' This is a .rds file containing all the edges data of two SBtab files
+#' The original SBtab files are physmap6 and physmap7
+#' The file has been created using the merge_sbtab() function
+#'
+#' @name merged_physmap67_edges
+#'
+#' @section merged_physmap67_edges.rds:
+#'
+#' This data is used in merged_net_graph().
+NULL
+
+#' The merged species tables from SBtab files physmap6 and physmap7
+#'
+#' This is a .rds file containing all the species data of two SBtab files
+#' The original SBtab files are physmap6 and physmap7
+#' The file has been created using the merge_sbtab() function
+#'
+#' @name merged_physmap67_species
+#'
+#' @section merged_physmap67_species.rds:
+#'
+#' This data is used in merged_net_graph().
+NULL
+
+#' The compartments table from SBtab file physmap6
+#'
+#' This is a .rds file containing the compartments of SBtab file physmap6
+#' The file has been created using the sep_sbtab() function
+#'
+#' @name physmap6_compartments
+#'
+#' @section physmap6_compartments.rds:
+#'
+#' This data is used in net_graph().
+NULL
+
+#' The edges table from SBtab file physmap6
+#'
+#' This is a .rds file containing the edges of SBtab file physmap6
+#' The file has been created using the sep_sbtab() function
+#'
+#' @name physmap6_edges
+#'
+#' @section physmap6_edges.rds:
+#'
+#' This data is used in net_graph().
+NULL
+
+#' The species table from SBtab file physmap6
+#'
+#' This is a .rds file containing the species of SBtab file physmap6
+#' The file has been created using the sep_sbtab() function
+#'
+#' @name physmap6_species
+#'
+#' @section physmap6_species.rds:
+#'
+#' This data is used in net_graph().
+NULL
+
+#' The reactions table from SBtab file physmap6 prepared for merging
+#'
+#' This is a .rds file containing the species of SBtab file physmap6
+#' The file has been prepared for merging using prep_merge()
+#' The dataframe has an extra column containing the original filename
+#' The ID's have been made unique by adding the original filename
+#'
+#' @name prepped_physmap6_reactions
+#'
+#' @section prepped_physmap6_reactions.rds:
+#'
+#' This data is used in merge_prepped().
+NULL
+
+#' The reactions table from SBtab file physmap7 prepared for merging
+#'
+#' This is a .rds file containing the species of SBtab file physmap7
+#' The file has been prepared for merging using prep_merge()
+#' The dataframe has an extra column containing the original filename
+#' The ID's have been made unique by adding the original filename
+#'
+#' @name prepped_physmap7_reactions
+#'
+#' @section prepped_physmap7_reactions.rds:
+#'
+#' This data is used in merge_prepped().
+NULL
